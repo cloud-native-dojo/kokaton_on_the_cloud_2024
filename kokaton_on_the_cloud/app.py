37efab298dc2df1161d8b84ba487f0ab5d67464a
nissy <85730730+nissy-code@users.noreply.github.com>
2024-10-15 10:20:49 +0900

    3: Allow External Access
    Flask Allow External Access

--- kokaton_on_the_cloud/app.py
+++ kokaton_on_the_cloud/app.py
@@ -50,4 +50,4 @@ def migrate_data():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # テーブルを作成
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
